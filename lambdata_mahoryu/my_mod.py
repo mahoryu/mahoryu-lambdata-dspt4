23f53daec3e9ae38b265cec00c904644d67f846c
Ethan Holden <mahoryu@gmail.com>
2020-03-11 21:37:33 -0600

    Added another function to my_mod.py

--- lambdata_mahoryu/my_mod.py
+++ lambdata_mahoryu/my_mod.py
@@ -1,17 +1,23 @@
 # lambdata_mahoryu/my_mod.py
 
+import pandas as pd
+
 def enlarge(n):
     return n * 100
 
-def split_date(X,date_column):
+def split_date(X, date_column):
     X.copy()
     X[date_column] = pd.to_datetime(X[date_column])
-    X['Year'] = X[date_column].dt.year
     X['Month'] = X[date_column].dt.month
     X['Day'] = X[date_column].dt.day
+    X['Year'] = X[date_column].dt.year
     X = X.drop(columns=date_column)
     return X
 
+def double_data(X):
+    X.copy()
+    X = X.append(X, ignore_index=True)
+    return X
 
 if __name__ == "__main__":
     # only if run from the command line, invoke the following,
--- lambdata_mahoryu/my_script.py
+++ lambdata_mahoryu/my_script.py
@@ -1,12 +1,20 @@
 # lambdata_mahoryu/my_script.py
 
-import pandas
+import pandas as pd
 print("Happy Tuesday Night")
 
 from lambdata_mahoryu.my_mod import enlarge
+from lambdata_mahoryu.my_mod import split_date
+from lambdata_mahoryu.my_mod import double_data
 
-df = pandas.DataFrame({"x":[1,2,3],"y":[4,5,6]})
+df = pd.DataFrame({"x":[1,2,3],"y":[4,5,6],"date":["1/2/2018","6/7/2019","2/8/2007"]})
 print(df.head())
 
 x = 5
 print ("ENLARGED",x, "TO", enlarge(x))
+
+print()
+print(split_date(df,"date"))
+
+print()
+print(double_data(df))
