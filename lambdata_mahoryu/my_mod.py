82046319c56e1a272e5ff74876bb66ca7d01694d
Ethan Holden <mahoryu@gmail.com>
2020-03-11 19:39:01 -0600

    Demonstrate import nuances

--- lambdata_mahoryu/my_mod.py
+++ lambdata_mahoryu/my_mod.py
@@ -2,3 +2,14 @@
 
 def enlarge(n):
     return n * 100
+
+
+if __name__ == "__main__":
+    # only if run from the command line, invoke the following,
+    # otherwise don't
+    
+    print("JUNK")
+    print("GLOBAL SCOPE")
+
+    y = float(input("Please input a number to enlarge"))
+    print(enlarge(y))
