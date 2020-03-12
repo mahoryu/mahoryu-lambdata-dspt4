5e8c8db77bcfb97c8e12ec9611c0bdd549c7398a
Ethan Holden <mahoryu@gmail.com>
2020-03-11 21:06:34 -0600

    Added function split_date

--- lambdata_mahoryu/my_mod.py
+++ lambdata_mahoryu/my_mod.py
@@ -3,6 +3,15 @@
 def enlarge(n):
     return n * 100
 
+def split_date(X,date_column):
+    X.copy()
+    X[date_column] = pd.to_datetime(X[date_column])
+    X['Year'] = X[date_column].dt.year
+    X['Month'] = X[date_column].dt.month
+    X['Day'] = X[date_column].dt.day
+    X = X.drop(columns=date_column)
+    return X
+
 
 if __name__ == "__main__":
     # only if run from the command line, invoke the following,
