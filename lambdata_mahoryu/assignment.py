5bbf0d159fb6641cf532439d29fa1e666ffac94a
mahoryu <mahoryu@gmail.com>
2020-03-16 21:21:32 -0600

    OOP with inheritance

--- lambdata_mahoryu/assignment.py
+++ lambdata_mahoryu/assignment.py
@@ -65,15 +65,44 @@ def double_data(X):
 #
 #     return X
 
-# OOP approach
+# # OOP approach
+#
+# class DataProcessor():
+#     def __init__(self, df):
+#         """
+#         :param df: df
+#         """
+#         self.df = df
+#
+#     def add_state_names(self):
+#         """
+#         Adds corresponding state names to a data frame
+#         :param X: the dataframe
+#         :param state_column: (string) the column name with the state abbreviations.
+#         :return: a new df with the new column
+#         """
+#
+#         # a map used for the conversion
+#         names_map = {
+#             "CA": "California",
+#             "CT": "Connecticut",
+#             "CO": "Colorado",
+#             "TX": "Texas",
+#             "DC": "Washington D.C.",
+#             "OH": "Ohio",
+#             "MI": "Missouri",
+#             "ID": "Idaho",
+#             "NY": "New York",
+#             "WA": "Washington",
+#         }
+#         self.df["State Names"] = self.df["abbrev"].map(names_map)
 
-class DataProcessor():
-    def __init__(self, df):
-        """
-        :param df: df
-        """
-        self.df = df
+# OOP approach with inheritance
 
+class CustomFrame(pd.DataFrame):
+    """
+    Notes on how this works
+    """
     def add_state_names(self):
         """
         Adds corresponding state names to a data frame
@@ -95,8 +124,7 @@ class DataProcessor():
             "NY": "New York",
             "WA": "Washington",
         }
-        self.df["State Names"] = self.df["abbrev"].map(names_map)
-
+        self["State Names"] = self["abbrev"].map(names_map)
 
 if __name__ == "__main__":
 
@@ -105,15 +133,20 @@ if __name__ == "__main__":
 
     # new_df = add_state_names(df, "abbrev")
     # print(new_df.head())
-    processor = DataProcessor(df1)
-    processor.add_state_names()
-    print(processor.df.head())
+    # processor = DataProcessor(df1)
+    # processor.add_state_names()
+    # print(processor.df.head())
+    #
+    # df2 = pd.DataFrame({"abbrev": ["OH", "MI", "ID", "NY", "WA"]})
+    # # print(df2.head())
+    #
+    # # new_df = add_state_names(df2, "abbrev")
+    # # print(new_df.head())
+    # processor = DataProcessor(df2)
+    # processor.add_state_names()
+    # print(processor.df.head())
 
-    df2 = pd.DataFrame({"abbrev": ["OH", "MI", "ID", "NY", "WA"]})
-    # print(df2.head())
-
-    # new_df = add_state_names(df2, "abbrev")
-    # print(new_df.head())
-    processor = DataProcessor(df2)
-    processor.add_state_names()
-    print(processor.df.head())
+    custom_df = CustomFrame({"abbrev":["CA","CT","CO","TX","DC"]})
+    print(custom_df.head())
+    custom_df.add_state_names()
+    print(custom_df.head())
