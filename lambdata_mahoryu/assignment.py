ad53514a71c6c9433f4a2681a2275666de8ea8e3
mahoryu <mahoryu@gmail.com>
2020-03-16 21:16:20 -0600

    OOP approach

--- lambdata_mahoryu/assignment.py
+++ lambdata_mahoryu/assignment.py
@@ -36,44 +36,84 @@ def double_data(X):
     X = X.append(X, ignore_index=True)
     return X
 
-def add_state_names(X, state_column):
-    """
-    Adds corresponding state names to a data frame
-    :param X: the dataframe
-    :param state_column: (string) the column name with the state abbreviations.
-    :return: a new df with the new column
-    """
+# Functional approach
+# def add_state_names(X, state_column):
+#     """
+#     Adds corresponding state names to a data frame
+#     :param X: the dataframe
+#     :param state_column: (string) the column name with the state abbreviations.
+#     :return: a new df with the new column
+#     """
+#
+#     # a map used for the conversion
+#     names_map = {
+#         "CA": "California",
+#         "CT": "Connecticut",
+#         "CO": "Colorado",
+#         "TX": "Texas",
+#         "DC": "Washington D.C.",
+#         "OH": "Ohio",
+#         "MI": "Missouri",
+#         "ID": "Idaho",
+#         "NY": "New York",
+#         "WA": "Washington",
+#     }
+#     # make a new data frame
+#     X = X.copy()
+#
+#     X["Full State Name"] = X[state_column].map(names_map)
+#
+#     return X
 
-    # a map used for the conversion
-    names_map = {
-        "CA": "California",
-        "CT": "Connecticut",
-        "CO": "Colorado",
-        "TX": "Texas",
-        "DC": "Washington D.C.",
-        "OH": "Ohio",
-        "MI": "Missouri",
-        "ID": "Idaho",
-        "NY": "New York",
-        "WA": "Washington",
-    }
-    # make a new data frame
-    X = X.copy()
+# OOP approach
 
-    X["Full State Name"] = X[state_column].map(names_map)
+class DataProcessor():
+    def __init__(self, df):
+        """
+        :param df: df
+        """
+        self.df = df
 
-    return X
+    def add_state_names(self):
+        """
+        Adds corresponding state names to a data frame
+        :param X: the dataframe
+        :param state_column: (string) the column name with the state abbreviations.
+        :return: a new df with the new column
+        """
+
+        # a map used for the conversion
+        names_map = {
+            "CA": "California",
+            "CT": "Connecticut",
+            "CO": "Colorado",
+            "TX": "Texas",
+            "DC": "Washington D.C.",
+            "OH": "Ohio",
+            "MI": "Missouri",
+            "ID": "Idaho",
+            "NY": "New York",
+            "WA": "Washington",
+        }
+        self.df["State Names"] = self.df["abbrev"].map(names_map)
+
 
 if __name__ == "__main__":
 
-    df = pd.DataFrame({"abbrev":["CA","CT","CO","TX","DC"]})
-    print(df.head())
+    df1 = pd.DataFrame({"abbrev":["CA","CT","CO","TX","DC"]})
+    # print(df1.head())
 
-    new_df = add_state_names(df, "abbrev")
-    print(new_df.head())
+    # new_df = add_state_names(df, "abbrev")
+    # print(new_df.head())
+    processor = DataProcessor(df1)
+    processor.add_state_names()
+    print(processor.df.head())
 
     df2 = pd.DataFrame({"abbrev": ["OH", "MI", "ID", "NY", "WA"]})
-    print(df2.head())
+    # print(df2.head())
 
-    new_df = add_state_names(df2, "abbrev")
-    print(new_df.head())
+    # new_df = add_state_names(df2, "abbrev")
+    # print(new_df.head())
+    processor = DataProcessor(df2)
+    processor.add_state_names()
+    print(processor.df.head())
